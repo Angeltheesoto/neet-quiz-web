df9befea80adc280de45434e1b0f907986f7f0a0
Angel Soto <100176750+Angeltheesoto@users.noreply.github.com>
2023-10-08 19:46:02 -0400

    Added more ads

--- client/src/components/questionItem/QuestionItem.js
+++ client/src/components/questionItem/QuestionItem.js
@@ -154,6 +154,20 @@ const QuestionItem = ({
     <div>
       <h3 data-theme={theme ? null : "dark"}>{quizName}</h3>
       {isEnd ? renderQuizEnd() : renderQuiz()}
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6889638865905659"
+        crossorigin="anonymous"
+      ></script>
+      <ins
+        class="adsbygoogle"
+        style={{ display: "block" }}
+        data-ad-client="ca-pub-6889638865905659"
+        data-ad-slot="5337783740"
+        data-ad-format="auto"
+        data-full-width-responsive="true"
+      ></ins>
+      <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
     </div>
   );
 };
--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -235,6 +235,20 @@ const Home = ({ fetchLsData, setFetchLsData, initializeLocalStorage }) => {
           />
         ))}
       </div>
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6889638865905659"
+        crossorigin="anonymous"
+      ></script>
+      <ins
+        class="adsbygoogle"
+        style={{ display: "block" }}
+        data-ad-client="ca-pub-6889638865905659"
+        data-ad-slot="5337783740"
+        data-ad-format="auto"
+        data-full-width-responsive="true"
+      ></ins>
+      <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
       <div className="home-quizname-container">
         {selectedQuizQuestions ? renderQuizQuestions() : renderQuizList()}
       </div>
